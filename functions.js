a0c9324708ab44316955eba78285875991a32537
Allison <allison.schroder.mail@gmail.com>
2023-07-21 16:40:38 -0700

    update to vertical transition

--- functions.js
+++ functions.js
@@ -1,49 +1,91 @@
-    xdomain = [0,6]
-    xrange = [0,200]
-    ydomain = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    yrange = [200,0]
-    xs = d3.scaleLinear().domain(xdomain).range(xrange)
-    ys = d3.scaleLinear().domain(ydomain).range(yrange)
-    xb = d3.scaleBand().domain([0, 1, 2, 3, 4, 5]).range(xrange)
-    yb = d3.scaleBand().domain(ydomain).range(yrange)
-    const data1 = [{year: 2013, ranking: ['Lab', 'Golden', 'Boxer', 'Beagle', 'GSD']}, {year: 2014, ranking: ['Lab', 'Golden', 'GSD', 'Frenchie', 'Poodle']}]
-    function loadInitialYearRanking() {
-      d3.select('svg').select('g').attr('transform', 'translate(50,50)')
-      d3.select('svg').select('g#chart').selectAll('rect').data(data1[0].ranking).enter().append('rect')
-        .attr('x', 20)
-        .attr('y', function(d,i) {return xb(i)})
-        .attr('width', function(d,i) {return 300 - i* 40})
-        .attr('height', 20)
-      d3.select('svg').select('g#chart').selectAll('text').data(data1[0].ranking).enter().append('text')
-        .attr('x', function(d,i) {return 250 - i* 40})
-        .attr('y', function(d,i) {return xb(i) + 15})
-        .attr('width', 100)
-        .attr('height', 20)
-        .text(function(d) {return d})
-      d3.select('svg').select('g#xaxis').attr('transform', 'translate(50,250)')
-      d3.select('svg').select('g#xaxis')
-        .attr('x', 20)
-        .call(d3.axisBottom(xb).tickValues([1, 2, 3, 4, 5]))
-    }
-    function loadYearData(year) {
-      const yearData = data1.filter(function(input) {return input.year === year})
-      const rankData = yearData[0].ranking
-      clearChartBars()
-      d3.select('svg').select('g#chart').selectAll('rect').data(rankData).enter().append('rect')
-        .attr('x', 20)
-        .attr('y', function(d,i) {return xb(i)})
-        .attr('width', function(d,i) {console.log(d); return 300 - i* 40})
-        .attr('height', 20)
-        .text('hi')
-      d3.select('svg').select('g#chart').selectAll('text').data(rankData).enter().append('text')
-        .attr('x', function(d,i) {console.log(d); return 250 - i* 40})
-        .attr('y', function(d,i) {return xb(i) + 15})
-        .attr('width', 100)
-        .attr('height', 20)
-        .text(function(d) {return d})
-    }
+const width = 400
+const height = 400
+const gMargin = 40
 
-    function clearChartBars(){
-      d3.select('svg').selectAll('rect').remove()
-      d3.select('svg').selectAll('text').remove()
-    }
+rankRange = [0,height]
+rankDomain = [1, 2, 3, 4, 5, 6]
+rb = d3.scaleBand().domain(rankDomain).range(rankRange)
+
+const data = [
+  {
+    breed: 'Labrador Retriever',
+    ranking: [{
+      year: 2013,
+      rank: 1
+    }, 
+    {
+      year: 2014,
+      rank: 2
+    },
+    {
+      year: 2015,
+      rank: 2
+    }]
+  },
+  {
+    breed: 'Beagle',
+    ranking: [{
+      year: 2013,
+      rank: 2
+    }, 
+    {
+      year: 2014,
+      rank: null
+    },
+    {
+      year: 2015,
+      rank: 1
+    }]
+  },
+  {
+    breed: 'Boxer',
+    ranking: [{
+      year: 2013,
+      rank: null
+    }, 
+    {
+      year: 2014,
+      rank: 1
+    },
+    {
+      year: 2015,
+      rank: null
+    }]
+  }
+]
+
+function loadInitialYearRanking(year) {
+  //setup svg size
+  d3.select('svg').attr('width', width).attr('height', height)
+
+  //setup g elements to contain elements to move together
+  const gSet = d3.select('svg').selectAll('g').data(data).enter().append('g')
+  .attr('id', function (d) {return d.breed})
+  .attr('transform', (d) => `translate(${gMargin}, ${getYByRank(d.ranking, year)})`);
+
+  //add rectangles to each
+  gSet.append('rect')
+  .attr('x', 0)
+  .attr('y', 0) 
+  .attr('width', 100)
+  .attr('height', 30) 
+  .attr('fill', 'purple')
+
+  //add text to each
+  gSet.append('text')
+  .attr('x', 10)
+  .attr('y', 20)
+  .text(function(d) {return d.breed})
+}
+
+function moveYearRanking(year) {
+  const gSet = d3.select('svg').selectAll('g')
+  gSet.transition().duration(1500)
+  .attr('transform', (d) => `translate(${gMargin}, ${getYByRank(d.ranking, year)})`);
+}
+
+function getYByRank(rankArray, year){
+  const ranking = rankArray.find((element) => element.year === year);
+  const rank = ranking.rank ? ranking.rank : 6;
+  return rb(rank)
+}
