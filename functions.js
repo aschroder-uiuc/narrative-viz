31b7d89a08a9a18c69afca1446cf2ce582b07c38
Allison <allison.schroder.mail@gmail.com>
2023-07-21 17:23:07 -0700

    first test adding annotations

--- functions.js
+++ functions.js
@@ -1,8 +1,9 @@
-const width = 400
+const width = 600
 const height = 400
 const gMargin = 40
+const barWidth = 175
 
-rankRange = [0,height]
+rankRange = [0,height + 100]
 rankDomain = [1, 2, 3, 4, 5, 6]
 rb = d3.scaleBand().domain(rankDomain).range(rankRange)
 
@@ -67,7 +68,7 @@ function loadInitialYearRanking(year) {
   gSet.append('rect')
   .attr('x', 0)
   .attr('y', 0) 
-  .attr('width', 100)
+  .attr('width', barWidth)
   .attr('height', 30) 
   .attr('fill', 'purple')
 
@@ -88,4 +89,47 @@ function getYByRank(rankArray, year){
   const ranking = rankArray.find((element) => element.year === year);
   const rank = ranking.rank ? ranking.rank : 6;
   return rb(rank)
+}
+
+//////////////////////////////////////
+// Annotations
+//////////////////////////////////////
+
+const type = d3.annotationCallout
+
+const annotations = [{
+  note: {
+    label: "I'm doing it!!!!",
+    bgPadding: {"top":15,"left":10,"right":10,"bottom":10},
+    title: "I have a thing to say"
+  },
+  //can use x, y directly instead of data
+ // data: { date: "18-Sep-09", close: 185.02 },
+ x: barWidth + gMargin,
+ y: getYByRank(data[1].ranking, 2013),
+  className: "show-bg",
+  dy: 137,
+  dx: 162,
+  connector: {
+    end: "arrow" // 'dot' also available
+  },
+}]
+
+const parseTime = d3.timeParse("%d-%b-%y")
+const timeFormat = d3.timeFormat("%d-%b-%y")
+
+//Skipping setting domains for sake of example
+const x = d3.scaleTime().range([0, 200])
+const y = d3.scaleLinear().range([200, 0])
+
+const makeAnnotations = d3.annotation()
+  .notePadding(15)
+  .type(type)
+  .annotations(annotations)
+
+function createAnnotations() {
+d3.select("svg")
+  .append("g")
+  .attr("class", "annotation-group")
+  .call(makeAnnotations)
 }
